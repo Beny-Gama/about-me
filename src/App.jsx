0bb96b0a9f3fedb74dd0aec530fc7883e0be784a
Bernardo Gama <76955720+Beny-Gama@users.noreply.github.com>
2024-08-10 10:52:32 -0400

    comesando o site

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './styles/components/app.sass';
+
+function App() {
+
+  return (
+    <>
+      <div id="protfolio">
+        <h1>Bernardo Gama</h1>
+      </div>
+  
+    </>
+  )
+}
+
+export default App
